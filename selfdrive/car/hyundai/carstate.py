496d65437673a8ea93fe760343f631edcf6e054c
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-07 06:26:26 -0400

    test 10 kph min

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -106,7 +106,7 @@ class CarState(CarStateBase):
 
         ret.cruiseState.standstill = cp_scc.vl["SCC11"]['SCCInfoDisplay'] == 4. if not self.no_radar else False
 
-        self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
+       # self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
 
         if ret.cruiseState.enabled:
             speed_conv = CV.MPH_TO_MS if self.is_set_speed_in_mph else CV.KPH_TO_MS
