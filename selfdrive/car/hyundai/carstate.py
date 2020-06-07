06c249d2d5966f2de9db57c80c37b239f777ba71
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-07 06:40:02 -0400

    4mph set speed

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -98,7 +98,7 @@ class CarState(CarStateBase):
         ret.steerWarning = cp_mdps.vl["MDPS12"]['CF_Mdps_ToiFlt'] != 0
 
         # cruise state
-        ret.cruiseState.enabled = (cp_scc.vl["SCC12"]['ACCMode'] != 0) if not self.no_radar else \
+        ret.cruiseState.enabled = ((cp_scc.vl["SCC12"]['ACCMode'] != 0) if not self.no_radar else \
             (cp.vl["LVR12"]['CF_Lvr_CruiseSet'] != 0)
 
         ret.cruiseState.available = (cp_scc.vl["SCC11"]["MainMode_ACC"] != 0) if not self.no_radar else \
@@ -106,11 +106,11 @@ class CarState(CarStateBase):
 
         ret.cruiseState.standstill = cp_scc.vl["SCC11"]['SCCInfoDisplay'] == 4. if not self.no_radar else False
 
-       # self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
+        self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
 
         if ret.cruiseState.enabled:
             speed_conv = CV.MPH_TO_MS if self.is_set_speed_in_mph else CV.KPH_TO_MS
-            ret.cruiseState.speed = cp_scc.vl["SCC11"]['VSetDis'] * speed_conv if not self.no_radar else \
+            ret.cruiseState.speed = (cp_scc.vl["SCC11"]['VSetDis'] - 16) * speed_conv if not self.no_radar else \
                 (cp.vl["LVR12"]["CF_Lvr_CruiseSet"] * speed_conv)
         else:
             ret.cruiseState.speed = 0
