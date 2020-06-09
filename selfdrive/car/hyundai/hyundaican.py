eec9b374f81061567076f74ffa5942b90380106c
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-09 10:06:25 -0400

    r

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -65,7 +65,8 @@ def create_clu11(packer, frame, bus, clu11, button, speed):
 
 def create_scc12(packer, apply_accel, enabled, cnt, scc12):
   values = scc12
-  if enabled and scc12["ACCMode"] == 1:
+  if enabled:
+    values["ACCMode"] = 1
     values["aReqRaw"] = apply_accel #aReqMax
     values["aReqValue"] = apply_accel #aReqMin
   values["CR_VSM_Alive"] = cnt
