65634b7f2533d21bf5bcbc0fc84a37a334f34ba8
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-09 12:52:26 -0400

    v

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -224,6 +224,7 @@ class CarInterface(CarInterfaceBase):
     ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
                                                                      else 2 if 1056 in fingerprint[2] else -1
     ret.radarOffCan = ret.sccBus == -1
+    ret.openpilotLongitudinalControl = True if not cs_out.brakePressed else False
     ret.enableCruise = not ret.radarOffCan or ret.sccBus != 0
     ret.autoLcaEnabled = False
 
@@ -236,8 +237,6 @@ class CarInterface(CarInterfaceBase):
 
     ret = self.CS.update(self.cp, self.cp2, self.cp_cam)
     ret.canValid = self.cp.can_valid and self.cp2.can_valid and self.cp_cam.can_valid
-
-    ret.openpilotLongitudinalControl = True if not ret.brakePressed else False
     self.CP.enableCruise = True
     # most HKG cars has no long control, it is safer and easier to engage by main on
     ret.cruiseState.enabled = ret.cruiseState.available
