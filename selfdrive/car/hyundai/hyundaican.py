4261849ac95bef8dcc9979d701ddd32d6ab4b96a
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-09 09:18:37 -0400

    c

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -1,4 +1,5 @@
 import crcmod
+from selfdrive.config import Conversions as CV
 from selfdrive.car.hyundai.values import CAR, CHECKSUM
 
 hyundai_checksum = crcmod.mkCrcFun(0x11D, initCrc=0xFD, rev=False, xorOut=0xdf)
