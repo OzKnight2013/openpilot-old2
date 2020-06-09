8645549a7b7d20ae1fdd6c6c857a7cdea4a4524d
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-09 01:00:06 -0400

    g

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -110,9 +110,9 @@ def create_lfa_mfa(packer, frame, enabled):
 
 def create_scc11(packer, frame, enabled, set_speed, lead_visible, scc11):
   values = scc11
-  values["MainMode_ACC"] = 1
   values["AliveCounterACC"] = frame % 0x10
   if enabled:
+    values["MainMode_ACC"] = 1
     values["VSetDis"] = set_speed / 3.6 # km/h velosity
     values["ObjValid"] = 1
     values["ACC_ObjStatus"] = lead_visible
