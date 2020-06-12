7d3fc0ce00bb02df6a3f909d0af8d287150960ab
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-12 09:32:23 -0400

    vc

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -78,6 +78,7 @@ class CarController():
     self.update_live = False
     self.scc_live = not CP.radarOffCan
     self.lead_visible = False
+    self.lead_debounce = 0
 
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert,
              left_lane, right_lane, left_lane_depart, right_lane_depart, set_speed, lead_visible):
