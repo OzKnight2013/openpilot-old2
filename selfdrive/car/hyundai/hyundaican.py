3e9839ac4cf5b71bca455d47f13c3d9eccafc343
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-13 16:19:38 -0400

    long

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -122,7 +122,7 @@ def create_scc11(packer, frame, enabled, set_speed, lead_visible, standstill, sc
   values["AliveCounterACC"] = frame // 2 % 0x10
   if enabled:
     values["VSetDis"] = set_speed
-  values["SCCInfoDisplay"] = 4 if standstill else 0
+#  values["SCCInfoDisplay"] = 4 if standstill  else 0 tie to long control state = stopping
   values["DriverAlertDisplay"] = 0
   values["ObjValid"] = lead_visible
   values["ACC_ObjStatus"] = lead_visible
--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -194,9 +194,9 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = car.CarParams.SteerControlType.torque
 
     ret.longitudinalTuning.kpBP = [0.  , 0.5,   5., 20., 40.]
-    ret.longitudinalTuning.kpV =  [0.1 , 0.15, 0.12, 0.1, 0.06]
+    ret.longitudinalTuning.kpV =  [0.1, 0.08, 0.07, 0.06, 0.05]
     ret.longitudinalTuning.kiBP = [0.]
-    ret.longitudinalTuning.kiV = [0.002]
+    ret.longitudinalTuning.kiV = [0.004]
     ret.longitudinalTuning.deadzoneBP = [0., 5.,  40]
     ret.longitudinalTuning.deadzoneV = [0.005, 0.01, 0.02]
 
