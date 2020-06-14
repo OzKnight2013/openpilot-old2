cb845278bb5dd4e0502d1cf010e8e3b069430c6f
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-13 23:23:52 -0400

    tune long

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -198,9 +198,9 @@ class CarInterface(CarInterfaceBase):
     #ret.longitudinalTuning.kiBP = [0.]
     #ret.longitudinalTuning.kiV = [0.002]
     ret.longitudinalTuning.kpBP = [0., 5., 35.]
-    ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+    ret.longitudinalTuning.kpV = [2., 1.6, 1.]
     ret.longitudinalTuning.kiBP = [0., 35.]
-    ret.longitudinalTuning.kiV = [0.18, 0.12]
+    ret.longitudinalTuning.kiV = [0.09, 0.06]
     ret.longitudinalTuning.deadzoneBP = [0., 5.,  40.]
     ret.longitudinalTuning.deadzoneV = [0.05, 0.08, 0.12]
 
