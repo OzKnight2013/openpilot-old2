1441636e590daf0cad59d3d004e59c935c4477af
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-15 22:30:29 -0400

    CHECK

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -42,7 +42,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kiV = [.065, .045, .045]
     ret.longitudinalTuning.deadzoneBP = [0., 5.,  40.]
     ret.longitudinalTuning.deadzoneV = [0.08, 0.12, 0.15]
-    ret.gasMaxBP = [0., 1., 5., 40]
+    ret.gasMaxBP = [0., 1., 5., 40.]
     ret.gasMaxV = [.15, .12, .15, .1] 
     ret.brakeMaxBP = [0., 5., 5.1]
     ret.brakeMaxV = [1., 1., 0.5]  # safety limits to stop unintended deceleration
