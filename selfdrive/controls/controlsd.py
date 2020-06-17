86ace7688ec140f0cf6fa104868d30fabf3336cf
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-16 20:01:33 -0400

    check

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -325,7 +325,7 @@ class Controls:
             self.state = State.enabled
           self.current_alert_types.append(ET.ENABLE)
           self.v_cruise_kph = initialize_v_cruise(CS.vEgo, CS.buttonEvents, self.v_cruise_kph_last)
-                
+
     self.v_cruise_kph_last = self.v_cruise_kph
 
     # Check if actuators are enabled
