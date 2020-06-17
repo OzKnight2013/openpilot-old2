558601dba1d63f7e7263f010773b7987bad80a3c
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-16 20:00:08 -0400

    this one may be

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -264,8 +264,6 @@ class Controls:
   def state_transition(self, CS):
     """Compute conditional state transitions and execute actions on state transitions"""
 
-    self.v_cruise_kph_last = self.v_cruise_kph
-
     # if stock cruise is completely disabled, then we can use our own set speed logic
     if not self.CP.enableCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, self.v_cruise_kph_last, CS.buttonEvents, self.enabled, self.is_metric)
@@ -327,6 +325,8 @@ class Controls:
             self.state = State.enabled
           self.current_alert_types.append(ET.ENABLE)
           self.v_cruise_kph = initialize_v_cruise(CS.vEgo, CS.buttonEvents, self.v_cruise_kph_last)
+                
+    self.v_cruise_kph_last = self.v_cruise_kph
 
     # Check if actuators are enabled
     self.active = self.state == State.enabled or self.state == State.softDisabling
