9583f07af96233eee3f1a09c9ec1d8e6035fa95d
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-18 11:38:46 -0400

    bug3

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -331,7 +331,7 @@ class CarInterface(CarInterfaceBase):
     if not self.CP.enableCruise:
       for b in self.buttonEvents:
         # do enable on both accel and decel buttons
-        if b.type in [ButtonType.accelCruise, ButtonType.decelCruise] and not b.pressed and self.CS.cruiseStateavailable:
+        if b.type in [ButtonType.accelCruise, ButtonType.decelCruise] and b.pressed and self.CS.cruiseStateavailable:
           events.add(EventName.buttonEnable)
           self.countenable += 1
           print("enable counter:~~~~~~~~", self.countenable)
