a399b68a02da7570dd42af0fa6a0d57b90df563d
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-22 14:59:34 -0400

    todo parkbrake state

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -192,7 +192,7 @@ class CarState(CarStateBase):
     self.scc11 = cp_scc.vl["SCC11"]
     self.scc12 = cp_scc.vl["SCC12"]
     self.mdps12 = cp_mdps.vl["MDPS12"]
-    ret.parkBrake = cp.vl["CGW1"]['CF_Gway_ParkBrakeSw']
+    self.parkBrake = cp.vl["CGW1"]['CF_Gway_ParkBrakeSw']
     self.steer_state = cp_mdps.vl["MDPS12"]['CF_Mdps_ToiActive'] #0 NOT ACTIVE, 1 ACTIVE
     ret.leadDistance = self.lead_distance = cp_scc.vl["SCC11"]['ACC_ObjDist'] if not self.no_radar else 0
     self.lkas_error = cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] == 7
