1b62fbcc5d9cce58982580d91f4f2d73b0a8e46a
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-23 10:36:04 -0400

    v

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -355,7 +355,7 @@ class CarController():
       self.error = (CS.out.vEgo - self.target)
       if self.error > 0:
         self.p_part = self.error * 0.1
-        self.i_part += self.error * 0.001
+        self.i_part += self.error * 0.01
       else:
         self.p_part = self.error * 0.2
         self.i_part += self.error * 0.01
