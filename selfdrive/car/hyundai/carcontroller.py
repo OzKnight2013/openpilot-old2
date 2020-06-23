a68cee89bb5420c1dfee40ee5a933a0c3183fa78
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-23 12:05:48 -0400

    v

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -361,7 +361,7 @@ class CarController():
       elif self.error < 0.: # release
         self.p_part =  self.error * 0.3
         self.i_part += self.error * 0.03
-      self.i_part = (self.i_part, 0.5)
+      self.i_part = min(self.i_part, 0.5)
       self.spas_accel = min(-(self.p_part + self.i_part), 0.)
     else:
       self.i_part = 0.
