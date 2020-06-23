669ba61295a40ee39c8e8e2b9baf27e109cd991f
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-23 09:05:47 -0400

    mask steering error

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -116,10 +116,10 @@ class CarInterfaceBase():
     #if cs_out.cruiseState.nonAdaptive:
     #  events.add(EventName.wrongCruiseMode)
 
-    if cs_out.steerError:
-      events.add(EventName.steerUnavailable)
-    elif cs_out.steerWarning:
-      events.add(EventName.steerTempUnavailable)
+    #if cs_out.steerError:
+    #  events.add(EventName.steerUnavailable)
+    #elif cs_out.steerWarning:
+    #  events.add(EventName.steerTempUnavailable)
 
     # Disable on rising edge of gas or brake. Also disable on brake when speed > 0.
     # Optionally allow to press gas at zero speed to resume.
