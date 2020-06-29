50c55825a0791d4e7b9c18ae4acf27b74d56e9df
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-06-29 13:06:43 -0400

    enable Auto Lane Change

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -160,6 +160,8 @@ class CarInterface(CarInterfaceBase):
                                                                          tire_stiffness_factor=tire_stiffness_factor)
 
     ret.enableCamera = is_ecu_disconnected(fingerprint[0], FINGERPRINTS, ECU_FINGERPRINT, candidate, Ecu.fwdCamera) or has_relay
+    ret.autoLcaEnabled = True
+
 
     return ret
 
