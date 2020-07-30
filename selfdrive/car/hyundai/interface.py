dac80c4f3284b9ba2ea43820a2d5593181df5c8e
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-07-30 13:24:41 -0400

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -269,10 +269,6 @@ class CarInterface(CarInterfaceBase):
 
     events = self.create_common_events(ret)
 
-    if abs(ret.steeringAngle) > 90. and EventName.steerTempUnavailable not in events.events:
-      events.add(EventName.steerTempUnavailable)
-    #if self.low_speed_alert and not self.CS.mdps_bus:
-    #  events.add(car.CarEvent.EventName.belowSteerSpeed)
     if self.CC.turning_indicator_alert:
       events.add(EventName.turningIndicatorOn)
     #if self.lkas_button_alert:
