842be24568c51a81d144dc41700028df2ee1053d
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-01 12:46:54 -0400

    409 for all cars

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -39,7 +39,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerLimitTimer = 0.8
     tire_stiffness_factor = 1.
 
-    ret.steermaxLimit = 255  # stock
+    ret.steermaxLimit = 409  # stock
 
     #Long tuning Params -  make individual params for cars, baseline Hyundai genesis
     ret.longitudinalTuning.kpBP = [0., 1., 10., 35.]
