95742c647d682dacc8a316aeda157af25e4ebed8
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-08 16:05:52 -0400

    update

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -75,12 +75,12 @@ class CarState(CarStateBase):
 
     self.cruise_main_button = int(cp.vl["CLU11"]["CF_Clu_CruiseSwMain"])
     self.cruise_buttons = int(cp.vl["CLU11"]["CF_Clu_CruiseSwState"])
-    self.lkas_button_on = (cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] != 0)
+    self.lkas_button_on = (cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] != 0) and (self.mdps_bus > 0)
     self.lkas_button_enable = 0
 
-    if self.lkas_button_on and not self.prev_lkas_button_on and (self.mdps_bus > 0):
+    if self.lkas_button_on and not self.prev_lkas_button_on:
       self.lkas_button_enable = 2
-    elif not self.lkas_button_on and self.prev_lkas_button_on and (self.mdps_bus > 0):
+    elif not self.lkas_button_on and self.prev_lkas_button_on:
       self.lkas_button_enable = 1
 
     # cruise state
