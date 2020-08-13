ab227df07802e68e96b091c053d4133ada0a20a8
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-12 21:30:47 -0400

    r

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -266,6 +266,6 @@ class CarInterface(CarInterfaceBase):
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
                                c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
                                c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
-                               c.hudControl.setSpeed, c.hudControl.leadVisible)
+                               )
     self.frame += 1
     return can_sends
