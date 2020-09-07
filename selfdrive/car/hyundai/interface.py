6519e2cf38226380298b8045924f6094dc6fefe7
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-07 11:31:28 -0400

    ok progress

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -203,7 +203,7 @@ class CarInterface(CarInterfaceBase):
     ret = self.CS.update(self.cp, self.cp2, self.cp_cam)
     ret.canValid = self.cp.can_valid and self.cp2.can_valid and self.cp_cam.can_valid
 
-    ret.cruiseState.enabled = ret.cruiseState.available and ret.radarOffCan
+    ret.cruiseState.enabled = ret.cruiseState.available and self.CC.nosccradar
 
     events = self.create_common_events(ret)
 
