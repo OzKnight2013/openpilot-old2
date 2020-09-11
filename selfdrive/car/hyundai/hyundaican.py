bcfa7d94ffbd78262a9f80c81e6d16a0ded6c6a3
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-11 15:16:04 -0400

    update

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -9,8 +9,8 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
                   left_lane, right_lane,
                   left_lane_depart, right_lane_depart, lfa_available, bus):
   values = lkas11
-  values["CF_Lkas_LdwsSysState"] = 3 if steer_req else sys_state
-  values["CF_Lkas_SysWarning"] = sys_warning
+  values["CF_Lkas_LdwsSysState"] = sys_state
+  values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0
   values["CF_Lkas_LdwsLHWarning"] = left_lane_depart
   values["CF_Lkas_LdwsRHWarning"] = right_lane_depart
   values["CR_Lkas_StrToqReq"] = apply_steer
