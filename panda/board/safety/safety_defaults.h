ea73dccb923852791907f8df3effb1c4da62ee89
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-15 21:37:48 -0400

    update

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -16,7 +16,7 @@ int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
      if ((addr == 688) && (mdps_msg_count == 2)){
        mdps_msg_count += 1
      }
-     if ((addr == 1296) || (addr == 524) || (addr == 1554)) && (mdps_msg_count > 0)){
+     if ((addr == 1296) || (addr == 524) || (addr == 1554) && (mdps_msg_count > 0)){
        mdps_msg_count -= 3
      }
      if (mdps_msg_count > 0) {
