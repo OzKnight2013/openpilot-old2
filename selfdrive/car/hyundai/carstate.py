4be2dd2d5128f3b771a506058d31e40399c2a9ae
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-27 20:10:06 -0400

    move fca check on bus2 only for scc harness

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -531,11 +531,7 @@ class CarState(CarStateBase):
           ("SCC11", 50),
           ("SCC12", 50),
         ]
-    if CP.fcaBus == 2:
-      signals += [
-        ("FCA_CmdAct", "FCA11", 0),
-        ("CF_VSM_Warn", "FCA11", 0),
-      ]
-      checks += [("FCA11", 50)]
+        if CP.fcaBus == 2:
+          checks += [("FCA11", 50)]
 
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)
