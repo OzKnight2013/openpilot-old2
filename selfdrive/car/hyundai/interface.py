de8a881759e5137d5b5d248bbe01d7012f2d1650
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-10-19 12:51:51 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -85,8 +85,8 @@ class CarInterface(CarInterfaceBase):
       ret.minSteerSpeed = 32 * CV.MPH_TO_MS
     elif candidate == CAR.HYUNDAI_GENESIS:
       ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGain = 2.
-      ret.lateralTuning.indi.outerLoopGain = 1.
+      ret.lateralTuning.indi.innerLoopGain = 2.5
+      ret.lateralTuning.indi.outerLoopGain = 1.5
       ret.lateralTuning.indi.timeConstant = 1.25
       ret.lateralTuning.indi.actuatorEffectiveness = 1.65
       ret.mass = 2060. + STD_CARGO_KG
