1c1349640691c920153976ccddd72c5894223c23
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-10-20 11:47:29 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -88,7 +88,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.innerLoopGain = 2.5  #stock is 3.0 but 4.0 seems good
       ret.lateralTuning.indi.outerLoopGain = 2.1  #stock is 2.0.  Trying out 2.5
       ret.lateralTuning.indi.timeConstant = 1.4  #Stock is 1.5.  1.3 is good
-      ret.lateralTuning.indi.actuatorEffectiveness = 1.3  #Stock is 1.0 1.4 is good 
+      ret.lateralTuning.indi.actuatorEffectiveness = 2.3  #Stock is 1.0 1.4 is good 
       ret.mass = 2060. + STD_CARGO_KG
       ret.wheelbase = 3.01
       ret.steerRatio = 16.5
