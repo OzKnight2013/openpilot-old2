cc0398fcb9d8629603a2c39a9a44e8005e03315a
AskAlice <alice@askalice.me>
2020-10-30 19:09:13 -0600

    kill athena

--- selfdrive/athena/manage_athenad.py
+++ selfdrive/athena/manage_athenad.py
@@ -19,18 +19,18 @@ def main():
   crash.bind_extra(version=version, dirty=dirty, is_eon=True)
   crash.install()
 
-  try:
-    while 1:
-      cloudlog.info("starting athena daemon")
-      proc = Process(name='athenad', target=launcher, args=('selfdrive.athena.athenad',))
-      proc.start()
-      proc.join()
-      cloudlog.event("athenad exited", exitcode=proc.exitcode)
-      time.sleep(5)
-  except Exception:
-    cloudlog.exception("manage_athenad.exception")
-  finally:
-    params.delete(ATHENA_MGR_PID_PARAM)
+  # try:
+  #   while 1:
+  #     cloudlog.info("starting athena daemon")
+  #     # proc = Process(name='athenad', target=launcher, args=('selfdrive.athena.athenad',))
+  #     # proc.start()
+  #     # proc.join()
+  #     # cloudlog.event("athenad exited", exitcode=proc.exitcode)
+  #     time.sleep(5)
+  # except Exception:
+  #   cloudlog.exception("manage_athenad.exception")
+  # finally:
+  #   params.delete(ATHENA_MGR_PID_PARAM)
 
 if __name__ == '__main__':
   main()
