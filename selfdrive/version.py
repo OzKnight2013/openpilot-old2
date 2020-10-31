d2d0de83da950147678df40913189a26cd4b06d9
AskAlice <alice@askalice.me>
2020-10-30 18:35:14 -0600

    mock remote

--- selfdrive/version.py
+++ selfdrive/version.py
@@ -6,6 +6,10 @@ from typing import List, Optional
 from common.basedir import BASEDIR
 from selfdrive.swaglog import cloudlog
 
+from common.op_params import opParams
+from common.travis_checker import travis
+
+cloak = opParams().get('mockRemote', True) if not travis else True
 
 def run_cmd(cmd: List[str]) -> str:
     return subprocess.check_output(cmd, encoding='utf8').strip()
@@ -19,18 +23,26 @@ def run_cmd_default(cmd: List[str], default: Optional[str] = None) -> Optional[s
 
 
 def get_git_commit(branch: str = "HEAD", default: Optional[str] = None) -> Optional[str]:
+  if cloak:
+    return "a3bef4c46274e16ca6d1b71c8ef5d02f942f7bd0"
   return run_cmd_default(["git", "rev-parse", branch], default=default)
 
 
 def get_git_branch(default: Optional[str] = None) -> Optional[str]:
+  if cloak:
+    return "release2"
   return run_cmd_default(["git", "rev-parse", "--abbrev-ref", "HEAD"], default=default)
 
 
 def get_git_full_branchname(default: Optional[str] = None) -> Optional[str]:
+  if cloak:
+    return "origin/release2"
   return run_cmd_default(["git", "rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}"], default=default)
 
 
 def get_git_remote(default: Optional[str] = None) -> Optional[str]:
+  if cloak:
+    return "https://github.com/commaai/openpilot.git"
   try:
     local_branch = run_cmd(["git", "name-rev", "--name-only", "HEAD"])
     tracking_remote = run_cmd(["git", "config", "branch." + local_branch + ".remote"])
@@ -55,32 +67,21 @@ branch = get_git_full_branchname()
 
 if (origin is not None) and (branch is not None):
   try:
-    comma_remote = origin.startswith('git@github.com:commaai') or origin.startswith('https://github.com/commaai')
-    tested_branch = get_git_branch() in ['devel', 'release2-staging', 'dashcam-staging', 'release2', 'dashcam']
+    if cloak:
+      comma_remote = origin.startswith('git@github.com:commaai') or origin.startswith('https://github.com/commaai')
+    else:
+      comma_remote = origin.startswith('git@github.com:arne182') or origin.startswith('https://github.com/arne182')
+    tested_branch = get_git_branch() in ['release2', 'release3', 'release4', 'release5', 'release6']
 
-    dirty = False
+    dirty = not comma_remote
+    if not cloak:
+      dirty = dirty or (subprocess.call(["git", "diff-index", "--quiet", branch, "--"]) != 0)
 
-    # Actually check dirty files
-    if not prebuilt:
-      # This is needed otherwise touched files might show up as modified
-      try:
-        subprocess.check_call(["git", "update-index", "--refresh"])
-      except subprocess.CalledProcessError:
-        pass
-      dirty = (subprocess.call(["git", "diff-index", "--quiet", branch, "--"]) != 0)
-
-      # Log dirty files
-      if dirty and comma_remote:
-        try:
-          dirty_files = run_cmd(["git", "diff-index", branch, "--"])
-          cloudlog.event("dirty comma branch", version=version, dirty=dirty, origin=origin, branch=branch,
-                         dirty_files=dirty_files, commit=get_git_commit(), origin_commit=get_git_commit(branch))
-        except subprocess.CalledProcessError:
-          pass
-
-    dirty = dirty or (not comma_remote)
-    dirty = dirty or ('master' in branch)
-
+    if dirty:
+      dirty_files = subprocess.check_output(["git", "diff-index", branch, "--"], encoding='utf8')
+      commit = subprocess.check_output(["git", "rev-parse", "--verify", "HEAD"], encoding='utf8').rstrip()
+      origin_commit = subprocess.check_output(["git", "rev-parse", "--verify", branch], encoding='utf8').rstrip()
+      cloudlog.event("dirty comma branch", version=version, dirty=dirty, origin=origin, branch=branch,
   except subprocess.CalledProcessError:
     dirty = True
     cloudlog.exception("git subprocess failed while checking dirty")
